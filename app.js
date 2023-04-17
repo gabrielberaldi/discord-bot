3b93f6d3f784f034efa5964960e3f8586e2919d1
Gabriel <gabriel.beraldi@perinity.com>
2023-04-17 14:28:58 -0300

    First commit

--- app.js
+++ app.js
@@ -10,49 +10,122 @@ import {
 import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
 import { getShuffledOptions, getResult } from './game.js';
 
-// Create an express app
 const app = express();
-// Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
-// Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
-// Store for in-progress games. In production, you'd want to use a DB
 const activeGames = {};
 
-/**
- * Interactions endpoint URL where Discord will send HTTP requests
- */
 app.post('/interactions', async function (req, res) {
-  // Interaction type and data
   const { type, id, data } = req.body;
 
-  /**
-   * Handle verification requests
-   */
   if (type === InteractionType.PING) {
     return res.send({ type: InteractionResponseType.PONG });
   }
 
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
   if (type === InteractionType.APPLICATION_COMMAND) {
     const { name } = data;
 
-    // "test" command
     if (name === 'test') {
-      // Send a message into the channel where command was triggered from
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          // Fetches a random emoji to send from a helper function
-          content: 'hello world ' + getRandomEmoji(),
+          content: 'Oi ' + getRandomEmoji(),
+        },
+      });
+    }
+
+    if (name === 'challenge' && id) {
+      const userId = req.body.member.user.id;
+      const objectName = req.body.data.options[0].value;
+      activeGames[id] = {
+        id: userId,
+        objectName,
+      };
+
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `Rock papers scissors challenge from <@${userId}>`,
+          components: [
+            {
+              type: MessageComponentTypes.ACTION_ROW,
+              components: [
+                {
+                  type: MessageComponentTypes.BUTTON,
+                  custom_id: `accept_button_${req.body.id}`,
+                  label: 'Accept',
+                  style: ButtonStyleTypes.PRIMARY,
+                },
+              ],
+            },
+          ],
         },
       });
     }
   }
+
+
+  // if (type === InteractionType.MESSAGE_COMPONENT) {
+  //   const componentId = data.custom_id;
+
+  //   if (componentId.startsWith('accept_button_')) {
+  //     const gameId = componentId.replace('accept_button_', '');
+  //     const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+  //     try {
+  //       await res.send({
+  //         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+  //         data: {
+  //           content: 'What is your object of choice?',
+  //           flags: InteractionResponseFlags.EPHEMERAL,
+  //           components: [
+  //             {
+  //               type: MessageComponentTypes.ACTION_ROW,
+  //               components: [
+  //                 {
+  //                   type: MessageComponentTypes.STRING_SELECT,
+  //                   custom_id: `select_choice_${gameId}`,
+  //                   options: getShuffledOptions(),
+  //                 },
+  //               ],
+  //             },
+  //           ],
+  //         },
+  //       });
+  //       await DiscordRequest(endpoint, { method: 'DELETE' });
+  //     } catch (err) {
+  //       console.error('Error sending message:', err);
+  //     }
+  //   } else if (componentId.startsWith('select_choice_')) {
+  //     const gameId = componentId.replace('select_choice_', '');
+  //     if (activeGames[gameId]) {
+  //       const userId = req.body.member.user.id;
+  //       const objectName = data.values[0];
+  //       const resultStr = getResult(activeGames[gameId], {
+  //         id: userId,
+  //         objectName,
+  //       });
+
+  //       delete activeGames[gameId];
+  //       const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+  //       try {
+  //         await res.send({
+  //           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+  //           data: { content: resultStr },
+  //         });
+  //         await DiscordRequest(endpoint, {
+  //           method: 'PATCH',
+  //           body: {
+  //             content: 'Nice choice ' + getRandomEmoji(),
+  //             components: [],
+  //           },
+  //         });
+  //       } catch (err) {
+  //         console.error('Error sending message:', err);
+  //       }
+  //     }
+  //   }
+  // }
 });
 
 app.listen(PORT, () => {
